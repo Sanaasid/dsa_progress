7f6ef9b83f4fd65f8e4e1161cf79d1861716f961
Sana Siddiqui <sanasid149@gmail.com>
2026-05-23 12:16:42 +0530

    Time: 44 ms (82.56%) | Memory: 24.4 MB (77.19%) - LeetSync

--- 1753-path-with-minimum-effort/path-with-minimum-effort.cpp
+++ 1753-path-with-minimum-effort/path-with-minimum-effort.cpp
@@ -9,9 +9,11 @@ public:
     int n=heights.size();
     int m= heights[0].size();
     vector<vector<int>> dist(n,vector<int>(m,1e9));
-    pair<int,int> source={0,0};
-    pair<int,int> destination = {n-1, n-1};
+    dist[0][0]=0;
+    pq.push({0,{0,0}});
     
+    int dr[]={-1,0,1,0};
+    int dc[]={0,1,0,-1};
     pq.push({0,{0,0}});
 dist[0][0] = 0;
     while(!pq.empty()){
@@ -20,8 +22,7 @@ dist[0][0] = 0;
         int diff= it.first;
         int r=it.second.first;
         int c=it.second.second;
-        int dr[]={-1,0,1,0};
-        int dc[]={0,1,0,-1};
+        
         if(r==n-1 && c==m-1) return diff;
         for(int i=0;i<4;i++){
             int newr=r+dr[i];
@@ -31,9 +32,7 @@ dist[0][0] = 0;
       if(newEffort<dist[newr][newc]){
         dist[newr][newc]=newEffort;
         pq.push({newEffort,{newr,newc}});
-      }      
-
-            
+      }           
            }
         }
     }
